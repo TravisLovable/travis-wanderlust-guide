b18ae16f862c45b1769cc43cb437a1c254e8322a
nickibjones <jonicki.b@gmail.com>
2026-05-11 21:22:36 -0300

    feat: design system foundations - tokens, animations, fonts, useMediaQuery

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export function useMediaQuery(query: string): boolean {
+  const get = () =>
+    typeof window !== "undefined" && window.matchMedia(query).matches;
+
+  const [matches, setMatches] = useState<boolean>(get);
+
+  useEffect(() => {
+    const mql = window.matchMedia(query);
+    const onChange = (e: MediaQueryListEvent) => setMatches(e.matches);
+    setMatches(mql.matches);
+    mql.addEventListener("change", onChange);
+    return () => mql.removeEventListener("change", onChange);
+  }, [query]);
+
+  return matches;
+}
